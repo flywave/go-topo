803caebdf5bdfb0f41109eb89da518421e9f998e
aninggo <aninggo@gmail.com>
2019-11-13 15:09:24 +0800

    change to static lib

--- topvis.go
+++ topvis.go
@@ -6,6 +6,9 @@ package topovis
 #cgo CFLAGS: -I ./
 #cgo linux CXXFLAGS: -I ./include -I ./include/linux -I ./
 #cgo darwin CXXFLAGS: -I ./include -I ./include/darwin -I ./
-#cgo windows CXXFLAGS:  -I ./include -I ./include/win64 -I ./
+#cgo windows CXXFLAGS:  -I ./include -I ./include/windows -I ./
+#cgo linux LDFLAGS: -L ./lib/linux_amd64 -lfreetype
+#cgo darwin LDFLAGS: -L ./lib/darwin_amd64 -lfreetype 
+#cgo windows LDFLAGS: -L ./lib/windows_amd64 -lfreetype
 */
 import "C"
