402669001bfd944071345aea70adabf8b3b126fd
hj <hawking2215@gmail.com>
2022-09-21 16:22:29 +0800

    update

--- ifc_test.go
+++ ifc_test.go
@@ -10,6 +10,8 @@ const TOLERANCE = 1e-8
 func TestIfc(t *testing.T) {
 	ts := IfcToTopoShape("tests/buildingElementProxy.ifc")
 	rev := NewMeshReceiver()
-	ts[0].Mesh(rev, TOLERANCE, 0.1, 0.5)
-	fmt.Println(len(rev.Tris))
+	for _, sh := range ts {
+		sh.Mesh(rev, TOLERANCE, 0.1, 0.5)
+		fmt.Println(len(rev.Tris))
+	}
 }
