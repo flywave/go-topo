93d9c407b9bffe57ebc5532849c8f087c9b2e743
hj <hawking2215@gmail.com>
2024-10-19 08:18:27 +0800

    update

--- ifc.go
+++ ifc.go
@@ -13,9 +13,6 @@ import "C"
 
 import (
 	"unsafe"
-
-	"github.com/flywave/go-mst"
-	vec3d "github.com/flywave/go3d/float64/vec3"
 )
 
 func init() {
@@ -57,58 +54,4 @@ func IsIfc(f string) bool {
 	fl := C.CString(f)
 	defer C.free(unsafe.Pointer(fl))
 	return bool(C.is_ifc_file(fl))
-}
-
-func TopoShapeToMst(shp []*Shape) (*mst.Mesh, *vec3d.Box, error) {
-	mesh := mst.NewMesh()
-	bbx := vec3d.MinBox
-	for _, shp := range shp {
-		node := &mst.MeshNode{}
-		if shp == nil || shp.IsNull() {
-			continue
-		}
-
-		rev := NewMeshReceiver()
-		res := int8(1)
-		res = shp.Mesh(rev, 1e-8, 1, 0.5)
-
-		if res != 0 {
-			continue
-		}
-		mtrg := &mst.MeshTriangle{Batchid: int32(len(mesh.Materials))}
-		for i := range rev.Tris {
-			ts := rev.Tris[i]
-			vcount := len(node.Vertices)
-			for _, v := range ts {
-				mtrg.Faces = append(mtrg.Faces, &mst.Face{Vertex: [3]uint32{uint32(v[0] + vcount), uint32(v[1] + vcount), uint32(v[2] + vcount)}})
-			}
-
-			vers := rev.Vers[i]
-			for _, v := range vers {
-				rawV := (vec3d.T)(v.Data())
-				node.Vertices = append(node.Vertices, [3]float32{float32(rawV[0]), float32(rawV[1]), float32(rawV[2])})
-				bbx.Extend(&rawV)
-			}
-			nm := rev.Norms[i]
-			for _, v := range nm {
-				d := v.Data()
-				node.Normals = append(node.Normals, [3]float32{float32(d[0]), float32(d[1]), float32(d[2])})
-			}
-			if rev.HasTexCoord() {
-				tc := rev.TexCoords[i]
-				for _, v := range tc {
-					d := v.Data()
-					node.TexCoords = append(node.TexCoords, [2]float32{float32(d[0]), float32(d[1])})
-				}
-			}
-		}
-		node.FaceGroup = append(node.FaceGroup, mtrg)
-
-		mtl := &mst.BaseMaterial{}
-		mtl.Color = [3]byte{255, 255, 255}
-		mtl.Transparency = 0.0
-		mesh.Materials = append(mesh.Materials, mtl)
-		mesh.Nodes = append(mesh.Nodes, node)
-	}
-	return mesh, &bbx, nil
 }
