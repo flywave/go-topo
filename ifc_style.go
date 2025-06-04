815dbc819553be26c40f3a6f0f3b01de950c0d22
aninggo <aninggo@gmail.com>
2025-06-04 10:05:32 +0800

    update

--- ifc_style.go
+++ ifc_style.go
@@ -16,7 +16,7 @@ func init() {
 
 	defaultMaterials["IfcSite"] = &IfcStyle{
 		Name:    "IfcSite",
-		Diffuse: IfcColor{0.75, 0.8, 0.65},
+		Diffuse: IfcColor{0.0, 102.0 / 255.0, 180.0 / 255.0},
 	}
 
 	defaultMaterials["IfcBuilding"] = &IfcStyle{
@@ -40,8 +40,9 @@ func init() {
 	}
 
 	defaultMaterials["IfcCurtainWall"] = &IfcStyle{
-		Name:    "IfcCurtainWall",
-		Diffuse: IfcColor{0.85, 0.85, 0.85},
+		Name:         "IfcCurtainWall",
+		Diffuse:      IfcColor{0.5, 0.5, 0.5},
+		Transparency: 0.5,
 	}
 
 	defaultMaterials["IfcDuctSegment"] = &IfcStyle{
@@ -81,28 +82,28 @@ func init() {
 
 	defaultMaterials["IfcSlab"] = &IfcStyle{
 		Name:    "IfcSlab",
-		Diffuse: IfcColor{0.4, 0.4, 0.4},
+		Diffuse: IfcColor{160.0 / 255.0, 150.0 / 255.0, 104.0 / 255.0},
 	}
 
 	defaultMaterials["IfcWallStandardCase"] = &IfcStyle{
 		Name:    "IfcWallStandardCase",
-		Diffuse: IfcColor{0.9, 0.9, 0.9},
+		Diffuse: IfcColor{150.0 / 255.0, 150.0 / 255.0, 104.0 / 255.0},
 	}
 
 	defaultMaterials["IfcWall"] = &IfcStyle{
 		Name:    "IfcWall",
-		Diffuse: IfcColor{0.9, 0.9, 0.9},
+		Diffuse: IfcColor{146.0 / 255.0, 72.0 / 255.0, 38.0 / 255.0},
 	}
 
 	defaultMaterials["IfcWindow"] = &IfcStyle{
 		Name:         "IfcWindow",
-		Diffuse:      IfcColor{0.75, 0.8, 0.75},
+		Diffuse:      IfcColor{0.2, 0.2, 0.8},
 		Transparency: 0.3,
 	}
 
 	defaultMaterials["IfcDoor"] = &IfcStyle{
 		Name:    "IfcDoor",
-		Diffuse: IfcColor{0.55, 0.3, 0.15},
+		Diffuse: IfcColor{0.637255, 0.603922, 0.670588},
 	}
 
 	defaultMaterials["IfcBeam"] = &IfcStyle{
@@ -126,15 +127,86 @@ func init() {
 	}
 
 	defaultMaterials["IfcSpace"] = &IfcStyle{
-		Name:         "IfcSpace",
-		Diffuse:      IfcColor{0.65, 0.75, 0.8},
-		Transparency: 0.8,
+		Name:    "IfcSpace",
+		Diffuse: IfcColor{0.5, 0.4, 0.1},
+	}
+
+	defaultMaterials["IfcStairFlight"] = &IfcStyle{
+		Name:    "IfcStairFlight",
+		Diffuse: IfcColor{0.637255, 0.603922, 0.670588},
+	}
+
+	defaultMaterials["IfcFlowSegment"] = &IfcStyle{
+		Name:    "IfcFlowSegment",
+		Diffuse: IfcColor{0.6, 0.4, 0.5},
+	}
+
+	defaultMaterials["IfcFurnishingElement"] = &IfcStyle{
+		Name:    "IfcFurnishingElement",
+		Diffuse: IfcColor{205.0 / 255.0, 104.0 / 255.0, 57.0 / 255.0},
+	}
+
+	defaultMaterials["IfcBuildingElementProxy"] = &IfcStyle{
+		Name:    "IfcBuildingElementProxy",
+		Diffuse: IfcColor{0.0, 0.8, 0.5},
+	}
+
+	defaultMaterials["IfcProxy"] = &IfcStyle{
+		Name:         "IfcProxy",
+		Diffuse:      IfcColor{0.0, 0.8, 0.5},
+		Transparency: 0.9,
+	}
+
+	defaultMaterials["IfcDistributionFlowElement"] = &IfcStyle{
+		Name:    "IfcDistributionFlowElement",
+		Diffuse: IfcColor{0.8470588235, 0.427450980392, 0.0},
+	}
+
+	defaultMaterials["IfcPile"] = &IfcStyle{
+		Name:    "IfcPile",
+		Diffuse: IfcColor{0.8470588235, 0.427450980392, 0.0},
+	}
+
+	defaultMaterials["IfcCovering"] = &IfcStyle{
+		Name:    "IfcCovering",
+		Diffuse: IfcColor{0.8470588235, 0.427450980392, 0.0},
+	}
+
+	defaultMaterials["IfcTransportElement"] = &IfcStyle{
+		Name:    "IfcTransportElement",
+		Diffuse: IfcColor{0.8470588235, 0.427450980392, 0.0},
+	}
+
+	defaultMaterials["IfcFlowController"] = &IfcStyle{
+		Name:    "IfcFlowController",
+		Diffuse: IfcColor{0.8470588235, 0.427450980392, 0.0},
+	}
+
+	defaultMaterials["IfcFlowFitting"] = &IfcStyle{
+		Name:    "IfcFlowFitting",
+		Diffuse: IfcColor{0.8470588235, 0.427450980392, 0.0},
+	}
+
+	defaultMaterials["IfcBuildingElementPart"] = &IfcStyle{
+		Name:    "IfcBuildingElementPart",
+		Diffuse: IfcColor{1.0, 0.5, 0.5},
+	}
+
+	defaultMaterials["IfcDistributionElement"] = &IfcStyle{
+		Name:    "IfcDistributionElement",
+		Diffuse: IfcColor{1.0, 0.5, 0.5},
+	}
+
+	defaultMaterials["IfcProduct"] = &IfcStyle{
+		Name:    "IfcProduct",
+		Diffuse: IfcColor{0.8470588235, 0.427450980392, 0.0},
 	}
 
 	defaultMaterial = &IfcStyle{
 		Name:    "DefaultMaterial",
 		Diffuse: IfcColor{0.7, 0.7, 0.7},
 	}
+
 }
 
 func GetIfcStyle(tp string) *IfcStyle {
